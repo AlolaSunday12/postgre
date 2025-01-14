2d4ef048542baee0949e08555bfcf789b1f84f07
AlolaSunday12 <alolasj@gmail.com>
2025-01-14 16:09:00 +0100

    update on book controller

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { BookService } from './book.service';
+import { CreateBookDto } from './dto/create-book.dto';
 
-@Controller('book')
-export class BookController {}
+@Controller('books')
+export class BookController {
+  constructor(private bookService: BookService) {}
+
+  @Get()
+  async getAllBooks() {
+    return this.bookService.findAll();
+  }
+
+  @Post()
+  async createBook(@Body() body: CreateBookDto) {
+    return this.bookService.create(body);
+  }
+
+  @Get(':id')
+  async getBook(@Param('id') id: string) {
+    return this.bookService.findOne(id);
+  }
+}
