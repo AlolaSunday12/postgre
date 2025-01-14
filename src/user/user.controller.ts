b656f31d97dc6fa03c9b3926ed13a4964a69e1e6
AlolaSunday12 <alolasj@gmail.com>
2025-01-15 00:32:26 +0100

    update on user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { SignUpDto } from './dto/signup.dto';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get()
+  async getAllUsers() {
+    return this.userService.findAll();
+  }
+
+  @Post()
+  async signUp(@Body() body: SignUpDto) {
+    return await this.userService.signup(body);
+  }
+}
