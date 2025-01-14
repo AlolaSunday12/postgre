5ae75c44cc1c416a9e0237095fbbd63eea1d69a3
AlolaSunday12 <alolasj@gmail.com>
2025-01-14 23:37:45 +0100

    update on user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { SignUpDto } from './dto/signup.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  signup(userDto: SignUpDto): Promise<User> {
+    const { name, email, password } = userDto;
+
+    const user = this.userRepository.create({ name, email, password });
+    return this.userRepository.save(user);
+  }
+
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+}
