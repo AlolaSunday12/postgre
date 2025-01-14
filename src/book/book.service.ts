10c24d542135ed3442206960e387ea50fab45bf6
AlolaSunday12 <alolasj@gmail.com>
2025-01-14 16:07:45 +0100

    update on book service

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Book } from './book.entity';
+import { Repository } from 'typeorm';
+import { CreateBookDto } from './dto/create-book.dto';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+  constructor(
+    @InjectRepository(Book) private bookRepository: Repository<Book>,
+  ) {}
+
+  findAll(): Promise<Book[]> {
+    return this.bookRepository.find();
+  }
+
+  create(bookDto: CreateBookDto): Promise<Book> {
+    const book = this.bookRepository.create(bookDto);
+
+    return this.bookRepository.save(book);
+  }
+
+  async findOne(id: string): Promise<Book> {
+    const book = await this.bookRepository.findOneBy({ id });
+
+    if (!book) {
+      throw new NotFoundException('Book not found');
+    }
+    return book;
+  }
+}
