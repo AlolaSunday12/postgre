48d5e831e4ef9ab6d1cbc0eb2b92b14c2c6f21a4
AlolaSunday12 <alolasj@gmail.com>
2025-01-14 16:10:10 +0100

    update on book entitiy

---
+++ src/book/book.entity.ts
@@ -0,0 +1,11 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+
+@Entity()
+export class Book {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string
+}
