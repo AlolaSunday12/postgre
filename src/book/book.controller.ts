8d23f142ef5d593ee13e596df67497b3df43b62a
AlolaSunday12 <alolasj@gmail.com>
2025-01-14 23:39:36 +0100

    update on book controller

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('books')
 export class BookController {
@@ -19,5 +20,15 @@ export class BookController {
   @Get(':id')
   async getBook(@Param('id') id: string) {
     return this.bookService.findOne(id);
+  }
+
+  @Put(':id')
+  async updateBook(@Param('id') id: string, @Body() body: UpdateBookDto) {
+    return this.bookService.update(id, body);
+  }
+
+  @Delete(':id')
+  async deleteBook(@Param('id') id: string) {
+    return this.bookService.delete(id);
   }
 }
